6e60373823b52a65ff93a7ad49321828f74d464a
林毅 <lnyielea@gmail.com>
2017-08-06 13:07:00 +0800

    添加output设置

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 import fs from "fs"
 import path from "path"
 export default class WebpackPluginTest {
+  constructor(options) {
+    this.options = Object.assign({output: "./fileMap.json"}, options);
+  }
   apply(complier) {
     complier.plugin("emit", (compilation, cb) => {
       let out, context;
       const fileMap = {};
       const assets = compilation.assets;
+      // console.log(Object.keys(compilation));
       compilation.chunks.map((chunk) => {
-        let loc, block;
-        chunk.blocks.map((b) => {
-          block = b;
-        });
-        if(block) {
-          let filePath;
+        if(chunk.name) {
+          fileMap[chunk.name] = chunk.files;
+        }
+        else {
+          let loc, block;
+          chunk.blocks.map((b) => {
+            block = b;
+          });
+          if(block) {
+            let filePath;
 
-          loc = block.loc;
-          context = block.parent.context;
-          filePath = path.join(context, loc).replace(process.env.PWD, "");
-          fileMap[filePath] = chunk.files;
+            loc = block.loc;
+            context = block.parent.context;
+            filePath = path.join(context, loc).replace(process.env.PWD, "");
+            fileMap[filePath] = chunk.files;
+          }
         }
       });
       out = JSON.stringify(fileMap);
-      fs.writeFile(path.join(process.env.PWD, "static/dist/fileMap.json"), out);
+      console.log("[fileMap output]", this.options.output);
+      fs.writeFile(this.options.output, out);
       cb();
     })
   }
